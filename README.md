cb49c2f0ad0bbd5ebda95f2fe1c22b6a88d10598
Dcods22 <Daniel.cody2@marist.edu>
2014-09-14 18:25:57 -0400

    Added functionality to store a cube

---
+++ README.md
@@ -0,0 +1,6 @@
+RubiksCube
+==========
+
+Rubiks Cube Directory for my AI Class
+
+Goal is to validate and solve a rubiks cube
--- src/main/java/rubik/Solvable.java
+++ src/main/java/rubik/Solvable.java
@@ -1,5 +1,10 @@
 package rubik;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Dan
@@ -7,6 +12,65 @@ package rubik;
  * Time: 5:08 PM
  */
 public class Solvable {
+
+    private static byte[] rubikCube;
+
+    public static void main(String args[]){
+        try{
+
+            rubikCube = new byte[54];
+
+            serialize(args[0]);
+
+            isValid(rubikCube);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void serialize(String file) throws FileNotFoundException {
+
+        String line = null;
+
+        try {
+
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            int pos = 0;
+
+            while((line = reader.readLine()) != null){
+                line = line.trim();
+                for(int i=0; i < line.length(); i++){
+                    String cube = line.substring(i, i+1);
+
+                    if (cube.equals("R")) {
+                        rubikCube[pos++] = 0;
+                    } else if (cube.equals("G")) {
+                        rubikCube[pos++] = 1;
+                    } else if (cube.equals("Y")) {
+                        rubikCube[pos++] = 2;
+                    } else if (cube.equals("B")) {
+                        rubikCube[pos++] = 3;
+                    } else if (cube.equals("O")) {
+                        rubikCube[pos++] = 4;
+                    } else if (cube.equals("W")) {
+                        rubikCube[pos++] = 5;
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void isValid(byte[] cube){
+
+    }
+
 
 
 }
