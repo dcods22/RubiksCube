02971c39fb7381e715b0ea1c008ab6ba86c1a101
Dcods22 <Daniel.cody2@marist.edu>
2014-09-15 10:04:31 -0400

    fixed tostring and added comments

--- src/main/java/rubik/Solvable.java
+++ src/main/java/rubik/Solvable.java
@@ -13,7 +13,10 @@ import java.io.IOException;
  */
 public class Solvable {
 
+    //reference to the cube array
     private static byte[] rubikCube;
+
+    //static ints for colors
     private static final int RED = 0;
     private static final int GREEN = 1;
     private static final int YELLOW = 2;
@@ -21,10 +24,17 @@ public class Solvable {
     private static final int ORANGE = 4;
     private static final int WHITE = 5;
 
+    /**
+     * constructor to create a solvable class
+     */
     public Solvable(){
         rubikCube = new byte[54];
     }
 
+    /**
+     * Main method to be called
+     * @param args the file of the cube to be executed on
+     */
     public static void main(String args[]){
         try{
 
@@ -40,7 +50,11 @@ public class Solvable {
 
     }
 
-
+    /**
+     * method to serialize the cube
+     * @param file of the cube to serialize
+     * @throws FileNotFoundException if the file is not found
+     */
     public void serialize(String file) throws FileNotFoundException {
         String line;
         int pos = 0;
@@ -75,10 +89,20 @@ public class Solvable {
         }
     }
 
+    /**
+     * Method to test the validitiy of a cube
+     * @param cube reference to the cube
+     * @return true if valid
+     */
     public boolean isValid(byte[] cube){
         return (count(cube) && middles(cube) && korf(cube));
     }
 
+    /**
+     * Method to test the count of colors of a cube
+     * @param cube reference to the cube
+     * @return true if valid
+     */
     public boolean count(byte[] cube){
 
         int rCount=0, gCount=0, yCount=0, bCount=0, oCount=0, wCount=0;
@@ -113,19 +137,70 @@ public class Solvable {
         return !(rCount != 9 && gCount != 9 && yCount != 9 && bCount != 9 && oCount != 9 && wCount != 9);
     }
 
+    /**
+     * Method to test the middle validitiy of a cube
+     * @param cube reference to the cube
+     * @return true if valid
+     */
     public boolean middles(byte[] cube){
         return ((cube[4] == 0) && (cube[19] == 1) && (cube[22] == 2) && (cube[25] == 3) && (cube[40] == 4) && (cube[49] == 5));
     }
 
+    /**
+     * Method to test the korf algorithm
+     * @param cube reference to the cube
+     * @return true if valid
+     */
     public boolean korf(byte[] cube){
+        return cornerTest(cube) && edgeTest(cube) && permutationTest(cube);
+    }
+
+    /**
+     * Method to test the corner validitiy of a cube
+     * @param cube reference to the cube
+     * @return true if valid
+     */
+    public boolean cornerTest(byte[] cube){
         return true;
     }
 
-    public String desearilize(byte[] cube){
+    /**
+     * Method to test the edge validitiy of a cube
+     * @param cube reference to the cube
+     * @return true if valid
+     */
+    public boolean edgeTest(byte[] cube){
+        return true;
+    }
+
+    /**
+     * Method to test the permutation validitiy of a cube
+     * @param cube reference to the cube
+     * @return true if valid
+     */
+    public boolean permutationTest(byte[] cube){
+        return true;
+    }
+
+    /**
+     * Method to deserialize the cube as it is stored
+     * @param cube copy of the cube to be deserialized
+     * @return a copy of the cube which is deserialized
+     */
+    public String deserialize(byte[] cube){
+        return "";
+    }
+
+    /**
+     * Overwrite of the toString method
+     * @return string printout of the current cube
+     */
+
+    public String toString(){
         String rubikString = "   ";
 
         for(int i=0; i < 9; i++){
-            rubikString += cube[i];
+            rubikString += rubikCube[i];
             if(i % 3 == 2 && i < 8){
                 rubikString += "\n   ";
             }else if(i % 3 == 2){
@@ -133,7 +208,7 @@ public class Solvable {
             }
         }
         for(int i = 9; i < 36; i++){
-            rubikString += cube[i];
+            rubikString += rubikCube[i];
             if(i % 9 == 8){
                 rubikString += "\n";
             }
@@ -142,17 +217,13 @@ public class Solvable {
         rubikString += "   ";
 
         for(int i = 36; i < 54; i++){
-            rubikString += cube[i];
+            rubikString += rubikCube[i];
             if(i % 3 == 2){
                 rubikString += "\n   ";
             }
         }
 
         return rubikString;
-    }
-
-    public String toString(){
-        return desearilize(rubikCube);
     }
 
 
