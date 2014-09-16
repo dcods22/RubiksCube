ec7eaa444080034869b21abf3d657c23431be666
Dcods22 <Daniel.cody2@marist.edu>
2014-09-16 15:04:32 -0400

    Added corner paradity test

--- src/main/java/rubik/RubikCube.java
+++ src/main/java/rubik/RubikCube.java
@@ -82,4 +82,152 @@ public class RubikCube {
         return rubikCube.length;
     }
 
+    /**
+     * Method to test the count of colors of a cube
+     * @return true if valid
+     */
+    public boolean count(){
+
+        int rCount=0, gCount=0, yCount=0, bCount=0, oCount=0, wCount=0;
+
+        if(this.getSize() < 54){
+            return false;
+        }
+
+        for(int i=0; i < this.getSize(); i++){
+            switch (this.getCube(i)) {
+                case 0:
+                    rCount++;
+                    break;
+                case 1:
+                    gCount++;
+                    break;
+                case 2:
+                    yCount++;
+                    break;
+                case 3:
+                    bCount++;
+                    break;
+                case 4:
+                    oCount++;
+                    break;
+                case 5:
+                    wCount++;
+                    break;
+            }
+        }
+
+        return !(rCount != 9 && gCount != 9 && yCount != 9 && bCount != 9 && oCount != 9 && wCount != 9);
+    }
+
+    /**
+     * Method to test the middle validitiy of a rubikCube
+     * @return true if valid
+     */
+    public boolean middles(){
+        return ((this.getCube(4) == 0) && (this.getCube(19) == 1) && (this.getCube(22) == 2) && (this.getCube(25) == 3) && (this.getCube(40) == 4) && (this.getCube(49) == 5));
+    }
+
+    /**
+     * Method to test the korf algorithm
+     * @return true if valid
+     */
+    public boolean korf(){
+        return cornerTest() && edgeTest() && permutationTest();
+    }
+
+    /**
+     * Method to test the corner validitiy of a rubikCube
+     * @return true if valid
+     */
+    public boolean cornerTest(){
+        int sum = rubikCube[0] + rubikCube[2] + rubikCube[6] + rubikCube[9] + rubikCube[11] + rubikCube[12] +
+                  rubikCube[14] + rubikCube[15] + rubikCube[17] + rubikCube[27] + rubikCube[29] + rubikCube[30] +
+                  rubikCube[32] + rubikCube[33] + rubikCube[35] + rubikCube[36] + rubikCube[38] + rubikCube[42] +
+                  rubikCube[44] + rubikCube[45] + rubikCube[47] + rubikCube[51] + rubikCube[53];
+
+        return ((sum % 3) == 0);
+    }
+
+    /**
+     * Method to test the edge validitiy of a rubikCube
+     * @return true if valid
+     */
+    public boolean edgeTest(){
+        return true;
+    }
+
+    /**
+     * Method to test the permutation validitiy of a rubikCube
+     * @return true if valid
+     */
+    public boolean permutationTest(){
+        return true;
+    }
+
+    /**
+     * Method to test the validitiy of a cube
+     * @return true if valid
+     */
+    public boolean validate(){
+        return (this.count() && this.middles() && this.korf());
+    }
+
+    /**
+     * function to return the interger to string
+     * @param value
+     * @return
+     */
+    public String returnToChar(int value){
+        switch(value){
+            case RED:
+                return "R";
+            case BLUE:
+                return "B";
+            case YELLOW:
+                return "Y";
+            case ORANGE:
+                return "O";
+            case WHITE:
+                return "W";
+            case GREEN:
+                return "G";
+        }
+        return "";
+    }
+
+    /**
+     * Overwrite of the toString method
+     * @return string printout of the current rubikCube
+     */
+    public String toString(){
+        String rubikString = "   ";
+
+        for(int i=0; i < 9; i++){
+            rubikString += this.returnToChar(this.getCube(i));
+            if(i % 3 == 2 && i < 8){
+                rubikString += "\n   ";
+            }else if(i % 3 == 2){
+                rubikString += "\n";
+            }
+        }
+        for(int i = 9; i < 36; i++){
+            rubikString += this.returnToChar(this.getCube(i));
+            if(i % 9 == 8){
+                rubikString += "\n";
+            }
+        }
+
+        rubikString += "   ";
+
+        for(int i = 36; i < 54; i++){
+            rubikString += this.returnToChar(this.getCube(i));
+            if(i % 3 == 2){
+                rubikString += "\n   ";
+            }
+        }
+
+        return rubikString;
+    }
+
 }
--- src/main/java/rubik/Solvable.java
+++ src/main/java/rubik/Solvable.java
@@ -40,97 +40,14 @@ public class Solvable {
 
     }
 
+
     /**
      * Method to test the validitiy of a cube
      * @param cube reference to the cube
      * @return true if valid
      */
     public boolean isValid(RubikCube cube){
-        return (count(cube) && middles(cube) && korf(cube));
-    }
-
-    /**
-     * Method to test the count of colors of a cube
-     * @param cube reference to the cube
-     * @return true if valid
-     */
-    public boolean count(RubikCube cube){
-
-        int rCount=0, gCount=0, yCount=0, bCount=0, oCount=0, wCount=0;
-
-        if(cube.getSize() < 54){
-            return false;
-        }
-
-        for(int i=0; i < cube.getSize(); i++){
-            switch (cube.getCube(i)) {
-                case 0:
-                    rCount++;
-                    break;
-                case 1:
-                    gCount++;
-                    break;
-                case 2:
-                    yCount++;
-                    break;
-                case 3:
-                    bCount++;
-                    break;
-                case 4:
-                    oCount++;
-                    break;
-                case 5:
-                    wCount++;
-                    break;
-            }
-        }
-
-        return !(rCount != 9 && gCount != 9 && yCount != 9 && bCount != 9 && oCount != 9 && wCount != 9);
-    }
-
-    /**
-     * Method to test the middle validitiy of a cube
-     * @param cube reference to the cube
-     * @return true if valid
-     */
-    public boolean middles(RubikCube cube){
-        return ((cube.getCube(4) == 0) && (cube.getCube(19) == 1) && (cube.getCube(22) == 2) && (cube.getCube(25) == 3) && (cube.getCube(40) == 4) && (cube.getCube(49) == 5));
-    }
-
-    /**
-     * Method to test the korf algorithm
-     * @param cube reference to the cube
-     * @return true if valid
-     */
-    public boolean korf(RubikCube cube){
-        return cornerTest(cube) && edgeTest(cube) && permutationTest(cube);
-    }
-
-    /**
-     * Method to test the corner validitiy of a cube
-     * @param cube reference to the cube
-     * @return true if valid
-     */
-    public boolean cornerTest(RubikCube cube){
-        return true;
-    }
-
-    /**
-     * Method to test the edge validitiy of a cube
-     * @param cube reference to the cube
-     * @return true if valid
-     */
-    public boolean edgeTest(RubikCube cube){
-        return true;
-    }
-
-    /**
-     * Method to test the permutation validitiy of a cube
-     * @param cube reference to the cube
-     * @return true if valid
-     */
-    public boolean permutationTest(RubikCube cube){
-        return true;
+        return cube.validate();
     }
 
     /**
@@ -146,35 +63,8 @@ public class Solvable {
      * Overwrite of the toString method
      * @return string printout of the current cube
      */
-
     public String toString(){
-        String rubikString = "   ";
-
-        for(int i=0; i < 9; i++){
-            rubikString += rubikCube.getCube(i);
-            if(i % 3 == 2 && i < 8){
-                rubikString += "\n   ";
-            }else if(i % 3 == 2){
-                rubikString += "\n";
-            }
-        }
-        for(int i = 9; i < 36; i++){
-            rubikString += rubikCube.getCube(i);
-            if(i % 9 == 8){
-                rubikString += "\n";
-            }
-        }
-
-        rubikString += "   ";
-
-        for(int i = 36; i < 54; i++){
-            rubikString += rubikCube.getCube(i);
-            if(i % 3 == 2){
-                rubikString += "\n   ";
-            }
-        }
-
-        return rubikString;
+        return rubikCube.toString();
     }
 
 
